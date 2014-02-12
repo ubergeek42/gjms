9460fff643f69ceac6751ea8b49f1b2062a01a80
Richard Blechinger <richard@blechi.at>
2014-02-12 23:38:19 +0100

    #gjmsdev - gjms.core.games.add finished.

--- core/database.py
+++ core/database.py
@@ -1,6 +1,6 @@
 import peewee
 
-database = peewee.SqliteDatabase("../data.db")
+database = peewee.SqliteDatabase("../dtd")
 
 class connector(peewee.Model):
 	class Meta:
--- core/games/__init__.py
+++ core/games/__init__.py
@@ -3,18 +3,35 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
 
-import gjms.core.database, gjms.util.password, gjms.core.exceptions, gjms.core.users, peewee
-
+import gjms.core.database, gjms.util.password, gjms.core.exceptions, gjms.core.users, peewee, playhouse.kv
 class Game(gjms.core.database.connector):
 	author = peewee.ForeignKeyField(gjms.core.users.User)
 	name = peewee.TextField()
+	description = peewee.TextField()
+	image = peewee.TextField()
+	platforms = playhouse.kv.KeyStore(peewee.TextField(null=True), database=gjms.core.database.database)
+
+	def add_platform(self, name, link):
+		Game.platforms[name] = link
 
 Game.create_table(fail_silently=True)
 
-def add(game, by):
-	new_game = Game.create(name=game, author=by)
+def add(game, by, desc, img):
+	new_game = Game.create(name=game, author=by, description=desc, image=img)
 	print "Added '%s' by %s." % (game, by.name)
 	return new_game
+
+def get(gamename):
+	try:
+		game = Game.get(Game.name == gamename)
+	except:
+		game = None
+
+	if type(game) is gjms.core.games.Game:
+		return game
+	else:
+		raise gjms.core.exceptions.NonExistentGame, "There is no game with that name."
+
 
 def by(author):
 	games = Game.select(Game, gjms.core.users.User).where(Game.author == author).join(gjms.core.users.User)
