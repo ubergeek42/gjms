f691ee96f242ed0e3e28d6ab95e657ffc7234f98
Richard Blechinger <richard@blechi.at>
2014-03-30 22:34:06 +0200

    #gjmsdev - Nothing exciting.

--- gjms/frontend/routes.py
+++ gjms/frontend/routes.py
@@ -15,9 +15,8 @@ import gjms.core.system
 import gjms.core.users
 import gjms.core.events
 import gjms.core.games
-import gjms.core.platforms
+import gjms.core.platforms DON GIOVA
 import gjms.core.ratings
-import gjms.core.system
 
 import gjms.util.report
 
@@ -25,10 +24,10 @@ from gjms.backend.app import app
 from gjms.config import parser
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
-system = gjms.core.system.get(1)
 
 @app.route("/")
 def gjms_root():
     """ The root route. """
-    return flask.render_template("frontend/base.html", time=datetime, system=system, config=parser, users=gjms.core.users, events=gjms.core.events, games=gjms.core.games, platforms=gjms.core.platforms, ratings=gjms.core.ratings)
+    system = gjms.core.system.get(1)
+    return flask.render_template("frontend/home.html", time=datetime, system=system, config=parser, users=gjms.core.users, events=gjms.core.events, games=gjms.core.games, platforms=gjms.core.platforms, ratings=gjms.core.ratings)
 gjms.util.report.output("Setup route /")
